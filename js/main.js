599a0ac90559dcfb0e51242d643cab8d08ba5bdf
Alper Ş <alpersahistan@gmail.com>
2021-07-13 20:15:27 +0300

    Switch to materialise css

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
+
+  (function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
